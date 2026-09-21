88794f061f3d41fe833bfb99b1b44766feb74844
brightsidepublication-stack <brightside.publication@gmail.com>
2026-09-21 19:56:17 +0330

    Create auth.types.ts

---
+++ src/types/auth.types.ts
@@ -0,0 +1,18 @@
+import type { User } from '@supabase/supabase-js';
+import type { UserRoleEnum } from './database.types';
+
+export interface UserProfile {
+  id: string;
+  role: UserRoleEnum;
+  email: string | null;
+  phone: string | null;
+  displayName: string | null;
+  avatarUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CurrentUser {
+  auth: User;
+  profile: UserProfile | null;
+}
